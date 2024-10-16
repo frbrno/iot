5694791d899e015f755d21b4111e6a5fa2b8d57a
frbrno <frbrno@proton.me>
2024-10-16 18:16:12 -0300

    [goofy_hawk] implement simple protocol lib

---
+++ node/goofy_hawk/lib/prot/conn_test.go
@@ -0,0 +1,72 @@
+package prot
+
+import (
+	"testing"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+const nats_url = "192.168.10.124:4222"
+const node_self = "goofy_hawk_test"
+const node_rusty_falcon = "rusty_falcon"
+
+func TestGet(t *testing.T) {
+	nc, err := nats.Connect(nats_url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer nc.Close()
+
+	conn := NewConn(node_self, nc)
+
+	t_begin := time.Now()
+	msg, err := conn.Get(node_rusty_falcon, "stepper1_state", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("success, reply_millis: %v, reply: %v", time.Since(t_begin).Milliseconds(), msg)
+}
+
+func TestRun(t *testing.T) {
+	nc, err := nats.Connect(nats_url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer nc.Close()
+
+	conn := NewConn(node_self, nc)
+
+	fn_cancel, sig_msg, err := conn.Run(node_rusty_falcon, "stepper1_move_to", []byte(`{"position_final": 20000,"step_delay_micros":1000}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fn_cancel()
+
+	// TODO: should this be done in conn.Run?
+	select {
+	case <-time.After(time.Second * 6):
+		t.Fatal("timeout waiting for ack")
+	case msg := <-sig_msg:
+		if msg.Typ != MsgTyp_Ack {
+			t.Fatal("protocol error, expected ack")
+		}
+	}
+
+	select {
+	case <-time.After(time.Minute * 2):
+		t.Fatal("timeout waiting for done")
+	case msg := <-sig_msg:
+		switch msg.Typ {
+		case MsgTyp_Done:
+			t.Log("success")
+		case MsgTyp_Cancel:
+			t.Log("action got canceled, who did this?")
+		case MsgTyp_Error:
+			t.Log("ups, got error")
+		default:
+			t.Log("protocol error")
+		}
+	}
+}
